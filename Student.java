39a7df0683c6843f0444da34309f614282965269
harshadkshirsagar619 <harshadkshirsagar619@gmail.com>
2026-05-07 23:34:37 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static  void main(String[] args)
+    {
+        int age = 23;
+        String name = "bob";
+        System.out.println("Hello world");
+
+        System.out.println("Student name : "+name + " And Age : "+age);
+    }
+}
