5057d5cebd9c6a06b3f12d13d06e7a88b5798342
harshadkshirsagar619 <harshadkshirsagar619@gmail.com>
2026-05-13 22:47:39 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+package String;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String name = "Harshad";
+
+        for (int i = 0;i<=name.length()-1;i++)
+        {
+            System.out.print(name.charAt(i));
+        }
+
+        for(int i = name.length()-1; i >= 0; i--)
+        {
+            System.out.print(name.charAt(i));
+        }
+    }
+}
